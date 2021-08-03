9b28c4bdbb6fbcc907eb5f197905da50b9daeae4
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-08-03 12:34:26 +0400

    Get rid of service interface

--- src/main/java/com/company/mvcfreemarker/service/UserService.java
+++ src/main/java/com/company/mvcfreemarker/service/UserService.java
@@ -1,9 +1,24 @@
 package com.company.mvcfreemarker.service;
 
 import com.company.mvcfreemarker.dto.UserDto;
+import com.company.mvcfreemarker.entity.User;
+import io.jmix.core.UnconstrainedDataManager;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface UserService {
-    List<UserDto> findAll();
+@Service
+public class UserService {
+    private UnconstrainedDataManager dataManager;
+
+    public UserService(UnconstrainedDataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public List<UserDto> findAll() {
+        return dataManager.load(User.class).all().list().stream()
+                .map(UserDto::new)
+                .collect(Collectors.toList());
+    }
 }
