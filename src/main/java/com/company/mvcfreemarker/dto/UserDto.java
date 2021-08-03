2690ffbc5d9e3f32b652372a298920531e549502
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-08-03 12:28:15 +0400

    Get rid of mapstruct

--- src/main/java/com/company/mvcfreemarker/dto/UserDto.java
+++ src/main/java/com/company/mvcfreemarker/dto/UserDto.java
@@ -1,8 +1,15 @@
 package com.company.mvcfreemarker.dto;
 
+import com.company.mvcfreemarker.entity.User;
+
 public class UserDto {
     private String username;
     private String email;
+
+    public UserDto(User user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
 
     public String getUsername() {
         return username;
--- src/main/java/com/company/mvcfreemarker/service/UserServiceImpl.java
+++ src/main/java/com/company/mvcfreemarker/service/UserServiceImpl.java
@@ -2,24 +2,24 @@ package com.company.mvcfreemarker.service;
 
 import com.company.mvcfreemarker.dto.UserDto;
 import com.company.mvcfreemarker.entity.User;
-import com.company.mvcfreemarker.mapper.UserMapper;
 import io.jmix.core.UnconstrainedDataManager;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
     private UnconstrainedDataManager dataManager;
-    private UserMapper userMapper;
 
-    public UserServiceImpl(UnconstrainedDataManager dataManager, UserMapper userMapper) {
+    public UserServiceImpl(UnconstrainedDataManager dataManager) {
         this.dataManager = dataManager;
-        this.userMapper = userMapper;
     }
 
     @Override
     public List<UserDto> findAll() {
-        return userMapper.fromEntities(dataManager.load(User.class).all().list());
+        return dataManager.load(User.class).all().list().stream()
+                .map(UserDto::new)
+                .collect(Collectors.toList());
     }
 }
